9e34a0e157df47665069240e331d6bfa3c1b3ee7
Sakthi Vikkraman R <sakthi900145@gmail.com>
2025-09-24 16:46:12 +0530

    folder structure added

--- template.sh
+++ template.sh
@@ -1,16 +1,13 @@
-# Creating directory 
 mkdir -p src
-mkdir -p research 
+mkdir -p research
 
-# Creating files
 touch src/__init__.py
-touch src/helper.py 
+touch src/helper.py
 touch src/prompt.py
-touch .env 
+touch .env
 touch setup.py
 touch app.py
-touch research/trials.ipynb 
-touch requirements.txt
+touch research/trails.ipynb
+touch requirement.txt
 
-
-echo "Directory and files created successfully!."
+echo "Files and directories created successfully."
