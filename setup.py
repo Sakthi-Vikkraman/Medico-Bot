55b592680cfe85493319c4b728f3337c2f711368
Sakthi Vikkraman R <sakthi900145@gmail.com>
2025-09-26 11:30:21 +0530

    requirements installed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Medico_Bot",
+    version="0.1.0",
+    author="Sakthi Vikkraman",
+    author_email="sakthi900145@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
